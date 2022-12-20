6a47a082a582e67dded0f34180dfb987b7ef265d
Juergen Zimmermann <Juergen.Zimmermann@HS-Karlsruhe.de>
2022-12-20 06:23:25 +0100

    Refactoring health module

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -18,10 +18,8 @@ import { HealthController } from './health.controller.js';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
-// import { healthConfig } from '../config/health.js';
 
 @Module({
-    // imports: [TerminusModule.forRoot({ errorLogStyle: 'pretty' }), HttpModule],
     imports: [TerminusModule, HttpModule],
     controllers: [HealthController],
 })
